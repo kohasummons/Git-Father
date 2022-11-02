09d144fd05e7588198334b46cf876cf905b137a4
Joshua Omobola <omobolathejoshua@gmail.com>
2022-11-01 20:05:41 -1100

    fix: confetti import

---
+++ Git Father/src/utils/confetti.js
@@ -0,0 +1,12 @@
+import JSConfetti from "js-confetti";
+const jsConfetti = new JSConfetti();
+
+export const config = {
+  emojis: ["🦄", "🌈", "⚡️", "💥", "✨", "💫", "🌸"],
+  emojiSize: 50,
+  confettiNumber: 15,
+};
+
+const yeehaw = jsConfetti;
+
+export default yeehaw;
